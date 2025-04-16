b7eae08b79a214d4b68b481f45d5f9774aee78a3
vdebenes <vanessa.debenest@gmail.com>
2025-04-16 12:11:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -139,7 +139,8 @@ if data:
         pdf.ln(row_height)
 
     pdf_buffer = io.BytesIO()
-    pdf.output(name=pdf_buffer)
+    pdf_bytes = pdf.output(dest='S').encode('latin1')
+    pdf_buffer.write(pdf_bytes)
     pdf_buffer.seek(0)
     st.download_button(
         label="📄 Télécharger tout en PDF",
