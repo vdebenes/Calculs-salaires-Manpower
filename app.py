d911166bb2071a9743ade9cc391478d508ef67ad
vdebenes <vanessa.debenest@gmail.com>
2025-04-22 11:40:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -60,16 +60,15 @@ def calcul_salaire(nom, date, tarif_horaire, heure_debut, heure_fin, pause, nume
             heures_dimanche += 1 / 60
         elif is_samedi:
             heures_samedi += 1 / 60
+        elif minute_count / 60 >= 9.5 and heures_nuit == 0 and heures_samedi == 0 and heures_dimanche == 0:
+            heures_sup += 1 / 60
         else:
-            if heures_nuit == 0 and heures_samedi == 0 and heures_dimanche == 0 and minute_count / 60 >= 9.5:
-                heures_sup += 1 / 60
-            else:
-                heures_normales += 1 / 60
+            heures_normales += 1 / 60
 
         current += timedelta(minutes=1)
         minute_count += 1
 
-    # Pas d'heures sup si une autre majoration est présente
+    # Supprimer les heures supplémentaires si des majorations ont déjà été comptées
     heures_sup_finales = 0.0
     if heures_nuit == 0 and heures_samedi == 0 and heures_dimanche == 0:
         heures_sup_finales = heures_sup
