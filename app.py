8ed64c636a01f2aefb515f7f0346da3578614b25
vdebenes <vanessa.debenest@gmail.com>
2025-04-22 13:18:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -54,14 +54,17 @@ def calcul_salaire(nom, date, tarif_horaire, heure_debut, heure_fin, pause, nume
         is_samedi = current.weekday() == 5
         is_nuit = h >= time(23, 0) or h < time(6, 0)
 
+        # Majoration nuit toujours comptée
         if is_nuit:
             heures_nuit += 1 / 60
-        if not is_nuit and is_dimanche:
-            heures_dimanche += 1 / 60
-        if not is_nuit and is_samedi:
-            heures_samedi += 1 / 60
-        if not (is_nuit or is_samedi or is_dimanche):
-            if minute_count / 60 >= 9.5:
+
+        # Majoration dimanche ou samedi comptée si pas dans la nuit
+        if not is_nuit:
+            if is_dimanche:
+                heures_dimanche += 1 / 60
+            elif is_samedi:
+                heures_samedi += 1 / 60
+            elif minute_count / 60 >= 9.5:
                 heures_sup += 1 / 60
             else:
                 heures_normales += 1 / 60
@@ -69,6 +72,7 @@ def calcul_salaire(nom, date, tarif_horaire, heure_debut, heure_fin, pause, nume
         current += timedelta(minutes=1)
         minute_count += 1
 
+    # Supprimer les heures sup si une autre majoration est déjà appliquée
     heures_sup_finales = 0.0
     if heures_nuit == 0 and heures_samedi == 0 and heures_dimanche == 0:
         heures_sup_finales = heures_sup
